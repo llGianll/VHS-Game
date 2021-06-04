681bcbaddeceb9d55b31ce9edf8bfc643b5296cb
emperorIgneous <william.jr.rosmarino@gmail.com>
2021-06-04 14:01:13 +0800

    added basic wallrun

--- Assets/Scripts/Movement Scripts/Rigidbody/RigidbodyPlayerMovement.cs
+++ Assets/Scripts/Movement Scripts/Rigidbody/RigidbodyPlayerMovement.cs
@@ -9,26 +9,53 @@ public class RigidbodyPlayerMovement : MonoBehaviour
     Transform orientation;
 
     [SerializeField]
+    Transform groundCheck;
+    [SerializeField]
     LayerMask groundMask;
-    public float groundDistance = 0.4f;
-    bool isGrounded;
-    
+    public float groundDistance = 0.2f;
+    public bool isGrounded;
+
+    Vector3 movementDirection;
+    Vector3 slopeMovementDirection;
+
+    public float playerSpeed = 12f;
+    public float playerWalkSpeed = 6f;
+    public float playerRunSpeed = 12f;
+    public float playerAcceleration = 10f;
+
+    public float playerSpeedMultiplier = 10f;
+    public float playerAirMultiplier = 0.4f;
+
+    public float playerJumpHeight = 3f;
+    public float playerGroundDrag = 12f;
+    public float playerAirDrag = 3f;
+
+    RaycastHit slopeHit;
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopeHit, playerHeight / 2 + 0.5f))
+        {
+            if(slopeHit.normal != Vector3.up)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
+    }
 
     float playerHeight;
 
     float xMovement;
     float zMovement;
 
-
-    Vector3 movementDirection;
-
-    public float playerSpeed = 12f;
-    public float playerSpeedMultiplier = 10f;
-    public float playerAirMultiplier = 0.4f; 
-
-    public float playerJumpHeight = 3f;
-    public float playerGroundDrag = 12f;
-    public float playerAirDrag = 3f;
+    
 
 
 
@@ -42,7 +69,7 @@ public class RigidbodyPlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        isGrounded = Physics.CheckSphere(transform.position - new Vector3(0, 1, 0), groundDistance, groundMask);
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         xMovement = Input.GetAxisRaw("Horizontal");
         zMovement = Input.GetAxisRaw("Vertical");
@@ -50,17 +77,32 @@ public class RigidbodyPlayerMovement : MonoBehaviour
         movementDirection = orientation.right * xMovement + orientation.forward * zMovement;
 
         DragControl();
+        SpeedControl();
 
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             playerJump();
         }
 
+        slopeMovementDirection = Vector3.ProjectOnPlane(movementDirection, slopeHit.normal);
+
     }
 
     private void FixedUpdate()
     {
         PlayerMove();
+    }
+
+    void SpeedControl()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            playerSpeed = Mathf.Lerp(playerSpeed, playerRunSpeed, playerAcceleration * Time.deltaTime);
+        }
+        else
+        {
+            playerSpeed = Mathf.Lerp(playerSpeed, playerWalkSpeed, playerAcceleration * Time.deltaTime);
+        }
     }
 
     void DragControl()
@@ -77,9 +119,13 @@ public class RigidbodyPlayerMovement : MonoBehaviour
 
     void PlayerMove()
     {
-        if (isGrounded)
+        if (isGrounded && !OnSlope())
         {
             rigidBody.AddForce(movementDirection.normalized * playerSpeed * playerSpeedMultiplier, ForceMode.Acceleration);
+        }
+        else if(isGrounded && OnSlope())
+        {
+            rigidBody.AddForce(slopeMovementDirection.normalized * playerSpeed * playerSpeedMultiplier, ForceMode.Acceleration);
         }
         else if (!isGrounded)
         {
@@ -90,6 +136,11 @@ public class RigidbodyPlayerMovement : MonoBehaviour
 
     void playerJump()
     {
-        rigidBody.AddForce(transform.up * playerJumpHeight, ForceMode.Impulse);
+        if (isGrounded)
+        {
+            Debug.Log("everybody jump");
+            rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
+            rigidBody.AddForce(transform.up * playerJumpHeight, ForceMode.Impulse);
+        }
     }
 }
---
+++ Assets/Scripts/Movement Scripts/Rigidbody/RigidbodyWallrun.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyWallrun : MonoBehaviour
+{
+    [SerializeField]
+    Transform orientation;
+
+    public float wallDistance;
+    public float minimumJumpHeight;
+
+    [SerializeField]
+    private float wallRunGravity;
+    [SerializeField]
+    private float wallRunJumpForce;
+
+    bool wallLeft = false;
+    bool wallRight = false;
+
+    RaycastHit leftWallHit;
+    RaycastHit rightWallHit;
+
+    private Rigidbody rigidBody;
+
+    bool CanWallRun()
+    {
+        return !Physics.Raycast(transform.position, Vector3.down, minimumJumpHeight);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WallCheck();
+
+        if (CanWallRun())
+        {
+            if (wallLeft)
+            {
+                StartWallRun();
+                Debug.Log("wall hit left");
+            }
+            else if(wallRight)
+            {
+                StartWallRun();
+                Debug.Log("wall hit right");
+            }
+            else
+            {
+                StopWallRun();
+            }
+        }
+        else
+        {
+            StopWallRun();
+        }
+    }
+
+    void WallCheck()
+    {
+        wallLeft = Physics.Raycast(transform.position, -orientation.right, out leftWallHit, wallDistance);
+        wallRight = Physics.Raycast(transform.position, orientation.right, out rightWallHit, wallDistance);
+    }
+
+    void StartWallRun()
+    {
+        rigidBody.useGravity = false;
+
+        rigidBody.AddForce(Vector3.down * wallRunGravity, ForceMode.Force);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (wallLeft)
+            {
+                Vector3 wallRunJumpDirection = transform.up + leftWallHit.normal; //check if normalize or not works better
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
+                rigidBody.AddForce(wallRunJumpDirection * wallRunJumpForce * 100, ForceMode.Force);
+            }
+            else if (wallRight)
+            {
+                Vector3 wallRunJumpDirection = transform.up + rightWallHit.normal; //check if normalize or not works better
+                rigidBody.velocity = new Vector3(rigidBody.velocity.x, 0, rigidBody.velocity.z);
+                rigidBody.AddForce(wallRunJumpDirection * wallRunJumpForce * 100, ForceMode.Force);
+            }
+        }
+    }
+
+    void StopWallRun()
+    {
+        rigidBody.useGravity = true;
+    }
+}
